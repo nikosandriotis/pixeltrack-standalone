5eeae5de7d2ddae2f247ba86677c67877b6e5082
Nikolaos Andriotis <nandriot@olice-05.cern.ch>
2022-07-26 14:34:11 +0200

    adding SiPixelFedCablingMapGPUWrapper stuff

---
+++ src/sycl/CondFormats/SiPixelFedCablingMapGPUWrapper.cc
@@ -0,0 +1,65 @@
+// C++ includes
+#include <algorithm>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+// SYCL includes
+#include <cuda_runtime.h>
+
+// CMSSW includes
+#include "SYCLCore/cudaCheck.h"
+#include "SYCLCore/device_unique_ptr.h"
+#include "SYCLCore/host_unique_ptr.h"
+#include "CondFormats/SiPixelFedCablingMapGPUWrapper.h"
+
+SiPixelFedCablingMapGPUWrapper::SiPixelFedCablingMapGPUWrapper(SiPixelFedCablingMapGPU const& cablingMap,
+                                                               std::vector<unsigned char> modToUnp)
+    : modToUnpDefault(modToUnp.size()), hasQuality_(true) {
+  //cudaCheck(cudaMallocHost(&cablingMapHost, sizeof(SiPixelFedCablingMapGPU)));
+  std::unique_ptr<SiPixelGainForHLTonGPU> cablingMapHost = std::make_unique<SiPixelFedCablingMapGPU>();
+  
+  std::memcpy(cablingMapHost, &cablingMap, sizeof(SiPixelFedCablingMapGPU));
+  std::copy(modToUnp.begin(), modToUnp.end(), modToUnpDefault.begin());
+}
+
+SiPixelFedCablingMapGPUWrapper::~SiPixelFedCablingMapGPUWrapper() { 
+  //cudaCheck(cudaFreeHost(cablingMapHost)); 
+  }
+
+const SiPixelFedCablingMapGPU* SiPixelFedCablingMapGPUWrapper::getGPUProductAsync(sycl::queue syclStream) const {
+  const auto& data = gpuData_.dataForCurrentDeviceAsync(syclStream, [this](GPUData& data, sycl::queue stream) {
+    // allocate
+    //cudaCheck(cudaMalloc(&data.cablingMapDevice, sizeof(SiPixelFedCablingMapGPU)));
+    data.cablingMapDevice = (SiPixelFedCablingMapGPU *)sycl::malloc_device(sizeof(SiPixelFedCablingMapGPU), stream);
+    // transfer
+    stream.memcpy(data.cablingMapDevice, this->cablingMapHost, sizeof(SiPixelFedCablingMapGPU));
+    //cudaCheck(cudaMemcpyAsync(
+    //    data.cablingMapDevice, this->cablingMapHost, sizeof(SiPixelFedCablingMapGPU), cudaMemcpyDefault, stream));
+ // });
+  return data.cablingMapDevice;
+}
+
+const unsigned char* SiPixelFedCablingMapGPUWrapper::getModToUnpAllAsync(sycl::queue syclStream) const {
+  const auto& data =
+      modToUnp_.dataForCurrentDeviceAsync(syclStream, [this](ModulesToUnpack& data, sycl::queue stream) {
+         data.modToUnpDefault = (unsigned char *)sycl::malloc_device(pixelgpudetails::MAX_SIZE_BYTE_BOOL, stream);
+         stream.memcpy(data.modToUnpDefault, this->modToUnpDefault.data(), this->modToUnpDefault.size() * sizeof(unsigned char)));
+        //cudaCheck(cudaMalloc((void**)&data.modToUnpDefault, pixelgpudetails::MAX_SIZE_BYTE_BOOL));
+        //cudaCheck(cudaMemcpyAsync(data.modToUnpDefault,
+        //                          this->modToUnpDefault.data(),
+        //                          this->modToUnpDefault.size() * sizeof(unsigned char),
+        //                          cudaMemcpyDefault,
+        //                          stream));
+      });
+  return data.modToUnpDefault;
+}
+
+SiPixelFedCablingMapGPUWrapper::GPUData::~GPUData() { 
+  //cudaCheck(cudaFree(cablingMapDevice)); 
+  }
+
+SiPixelFedCablingMapGPUWrapper::ModulesToUnpack::~ModulesToUnpack() { 
+  //cudaCheck(cudaFree(modToUnpDefault)); 
+  }
---
+++ src/sycl/CondFormats/SiPixelFedCablingMapGPUWrapper.h
@@ -0,0 +1,46 @@
+#ifndef RecoLocalTracker_SiPixelClusterizer_SiPixelFedCablingMapGPUWrapper_h
+#define RecoLocalTracker_SiPixelClusterizer_SiPixelFedCablingMapGPUWrapper_h
+
+#include "SYCLCore/ESProduct.h"
+#include "SYCLCore/HostAllocator.h"
+#include "SYCLCore/device_unique_ptr.h"
+#include "CondFormats/SiPixelFedCablingMapGPU.h"
+
+#include <CL/sycl.hpp>
+
+#include <set>
+
+class SiPixelFedCablingMapGPUWrapper {
+public:
+  explicit SiPixelFedCablingMapGPUWrapper(SiPixelFedCablingMapGPU const &cablingMap,
+                                          std::vector<unsigned char> modToUnp);
+  ~SiPixelFedCablingMapGPUWrapper();
+
+  bool hasQuality() const { return hasQuality_; }
+
+  // returns pointer to GPU memory
+  const SiPixelFedCablingMapGPU *getGPUProductAsync(sycl::queue stream) const;
+
+  // returns pointer to GPU memory
+  const unsigned char *getModToUnpAllAsync(sycl::queue stream) const;
+
+private:
+  std::vector<unsigned char, cms::sycltools::HostAllocator<unsigned char>> modToUnpDefault;
+  bool hasQuality_;
+
+  SiPixelFedCablingMapGPU *cablingMapHost = nullptr;  // pointer to struct in CPU
+
+  struct GPUData {
+    ~GPUData();
+    SiPixelFedCablingMapGPU *cablingMapDevice = nullptr;  // pointer to struct in GPU
+  };
+  cms::sycltools::ESProduct<GPUData> gpuData_;
+
+  struct ModulesToUnpack {
+    ~ModulesToUnpack();
+    unsigned char *modToUnpDefault = nullptr;  // pointer to GPU
+  };
+  cms::sycltools::ESProduct<ModulesToUnpack> modToUnp_;
+};
+
+#endif
